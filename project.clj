6db9bdb8b4b3984bba681094ec906a8e43ab4221
Doug Selph <dselph@gmail.com>
2013-05-04 23:24:16 -0500

    Tutorial 3 done

--- project.clj
+++ project.clj
@@ -4,9 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :source-paths ["src/clj"]
-  :dependencies [[org.clojure/clojure "1.5.1"]]
-  :plugins [[lein-cljsbuild "0.3.0"]]
-
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [compojure "1.1.5"]]
+  :ring {:handler modern-cljs.core/handler}
   :cljsbuild {:builds
               [{:source-paths ["src/cljs"]
 
