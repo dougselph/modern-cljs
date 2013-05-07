9350f7594790e1a1eb4c00dab33adfa21678adb6
Doug Selph <dselph@gmail.com>
2013-05-06 22:20:59 -0500

    Tutorial 6 done

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :source-paths ["src/clj"]
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [compojure "1.1.5"]]
+                 [compojure "1.1.5"]
+                 [domina "1.0.2-SNAPSHOT"]]
   :ring {:handler modern-cljs.core/handler}
   :cljsbuild {:builds
               [{:source-paths ["src/cljs"]
--- src/cljs/modern_cljs/login.cljs
+++ src/cljs/modern_cljs/login.cljs
@@ -1,19 +1,20 @@
-(ns modern-cljs.login)
+(ns modern-cljs.login
+  (:use [domina :only [by-id value]]))
 
-;; define the function to be attached to form submission event
 (defn validate-form []
-  ;; get email and password element from their ids in the HTML form
-  (let [email (.getElementById js/document "email")
-        password (.getElementById js/document "password")]
-    (if (and (> (count (.-value email)) 0)
-             (> (count (.-value password)) 0))
+  ;; get email and password element using (by-id id)
+  (let [email (by-id "email")
+        password (by-id "password")]
+    ;; get email and password value using (value el)
+    (if (and (> (count (value email)) 0)
+             (> (count (value password)) 0))
       true
       (do (js/alert "Please, complete the form!")
-          false))))
+                    false))))
 
 ;; define the function to attach validate-form to onsubmit event of
 ;; the form
-(defn init []
+(defn ^:export init []
   ;; verify that js/document exists and that it has a getElementById
   ;; property
   (if (and js/document
@@ -25,4 +26,5 @@
       (set! (.-onsubmit login-form) validate-form))))
 
 ;; initialize the HTML page in unobtrusive way
-(set! (.-onload js/window) init)
+;;(set! (.-onload js/window) init)
+
